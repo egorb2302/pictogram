bcabe70224c23b07ea37c525806888b411530a10
href <berendeevegor@gmail.com>
2026-07-02 19:01:28 +0400

    fix submit for re-creating pics

--- app/identidock.py
+++ app/identidock.py
@@ -1,29 +1,36 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
-
+import hashlib
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def identiicon():
     name = default_name
 
+    if request.method == 'POST':
+        name = request.form['name']
+    
+    salted_name = salt + name
+    name_hash = hashlib.sha256( salted_name.encode() ).hexdigest()
+
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-              Hello <input type="text" name="name" value="{}">
+              Hello <input type="text" name="name" value="{0}">
               <input type="submit" value="submit">
               </form>
               <p>You look like a: </p>
-              <img src="/monster/{}" />
-           '''.format(name, name)
+              <img src="/monster/{1}" />
+           '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer
 
 @app.route('/monster/<name>')
 def get_identiicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=120')
     image = r.content
 
     return Response(image, mimetype='image/png')
